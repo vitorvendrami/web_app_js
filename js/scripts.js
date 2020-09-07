0fe8c539589e2cd5bbeb67c7100ec2ce1fb7c866
Vitor_Vendrami <vitor_vendrami@hotmail.com>
2020-09-07 13:56:24 -0300

    add initial templates

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+
+$(function(){
+
+
+// *** APIs ***
+// clima, previsão 12 horas e previsão 5 dias: https://developer.accuweather.com/apis
+// pegar coordenadas geográficas pelo nome da cidade: https://docs.mapbox.com/api/
+// pegar coordenadas do IP: http://www.geoplugin.net
+// gerar gráficos em JS: https://www.highcharts.com/demo
+
+
+
+});
